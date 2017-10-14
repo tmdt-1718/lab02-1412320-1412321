ea105be1aacd0f792c5fd845445d4575f0e14590
minhle <minhle@teqnological.asia>
2017-10-14 15:55:09 +0700

    User sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'users/sessions#new'
+  root 'users#home'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  resources :users, only: [ :member ] do
+  	get 'home'
+  end
 end
