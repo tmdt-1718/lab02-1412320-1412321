8489a2773378ae9b26adf28bd65c20381b2e9acc
1412321 <minhluong96@gmail.com>
2017-10-15 18:38:02 +0700

    Modeling Messages & Conversations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create([
+  { 
+    name: "Luong Nhat Minh", 
+    email: "minhluong96@gmail.com" ,
+    password: "1234567", 
+    password_confirmation: "1234567" 
+  },
+  { 
+    name: "Le Quoc Minh", 
+    email: "quocminh97@gmail.com" ,
+    password: "1234567", 
+    password_confirmation: "1234567" 
+  },{ 
+    name: "Nguyen Thi Bao Ngoc", 
+    email: "ngoc120742@gmail.com" ,
+    password: "1234567", 
+    password_confirmation: "1234567" 
+  }
+])
