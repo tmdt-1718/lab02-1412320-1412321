737dcf0bb3018ef3b6eb2a5410d15b226492a7b3
1412321 <minhluong96@gmail.com>
2017-10-17 01:32:29 +0700

    Show message's sent time

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,6 @@
 module MessagesHelper
+  def localtime(time)
+    local = time.localtime();
+    return "<span class='time'>#{local.day}/#{local.month} #{local.hour}:#{local.min}</span>".html_safe
+  end
 end
