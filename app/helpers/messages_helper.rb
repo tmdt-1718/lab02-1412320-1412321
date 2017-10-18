306c703d173927ed6b78a5d512e84d980d7b5c92
1412321 <minhluong96@gmail.com>
2017-10-18 17:21:36 +0700

    Style for messages seen_at attribute

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,6 @@
 module MessagesHelper
   def localtime(time)
     local = time.localtime();
-    return "<span class='time'>#{local.day}/#{local.month} #{local.hour}:#{local.min}</span>".html_safe
+    return "#{local.day}/#{local.month} #{local.hour}:#{local.min}"
   end
 end
