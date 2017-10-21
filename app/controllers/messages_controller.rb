6f285337b63e121b5d39afe214bc4c04a56ba588
1412321 <minhluong96@gmail.com>
2017-10-21 15:41:26 +0700

    Order message by created_at

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :correct_user, only: [:show]
+
   def create
     if(params[:conversation_id])
       @conversation = Conversation.includes(:recipient).find(params[:conversation_id])
@@ -16,9 +18,10 @@ class MessagesController < ApplicationController
   def index
     # For loading messages
     conversations = Conversation.get_all(current_user.id)
-    @messages = conversations.collect(&:messages).flatten.uniq
+    @messages = conversations.collect(&:messages)
+    @messages.flatten!.uniq!
     @messages.delete_if { |message| message.user == current_user }
-    
+    @messages.sort!{ |a, b| b.created_at <=> a.created_at }
     # For create new message
     @message = Message.new
     @users = User.where.not(id: current_user.id)
@@ -34,24 +37,32 @@ class MessagesController < ApplicationController
   end
 
   def show
-    begin
-      @message = Message.find(params[:id])
-      @recipient = @message.conversation.opposed_user(@message.user)
-    rescue ActiveRecord::RecordNotFound => e
-      head 404
-    else
-      if (@message.user != current_user) && (!@message.seen_at) 
-        @message.update(seen_at: DateTime.now)
+    if (@message.user != current_user) && (!@message.seen_at) 
+      @message.update(seen_at: DateTime.now)
+    end
+    respond_to do |format|
+      format.html do
+        redirect_to messages_path
       end
-      respond_to do |format|
-        format.html
-        format.js
-      end
+      format.js
     end
   end
 
   private
     def message_params
       params.require(:message).permit(:user_id, :content)
+    end
+
+    def correct_user
+      begin
+        @message = Message.find(params[:id])
+        @recipient = @message.conversation.opposed_user(@message.user)
+      rescue ActiveRecord::RecordNotFound => e
+        head 404
+      else
+        if (@message.user != current_user) && (@recipient != current_user)
+          redirect_to '/404' and return
+        end
+      end        
     end
 end
